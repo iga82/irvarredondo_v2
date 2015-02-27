3296297cf92696b8c39098b7106aa0002b9120ea
Irving Arredondo <irva91@gmail.com>
2015-02-27 13:21:17 -0800

    renamed index to home

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Irving Arredondo' });
+  res.render('home', { title: 'Irving Arredondo' });
 });
 
 module.exports = router;
