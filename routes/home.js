f18a893fcc741c5e03fe792ef62c3eec3a16ccb9
Irving Arredondo <irva91@gmail.com>
2015-02-27 15:58:27 -0800

    have navbar with dynamic active button working

--- routes/home.js
+++ routes/home.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res) {
   res.render('home', { title: 'Irving Arredondo' });
 });
 
+router.get('/about', function(req, res){
+  res.render('about', { title: 'About' });
+});
+
+router.get('/portfolio', function(req, res){
+  res.render('portfolio', { title: 'Portfolio' });
+});
+
 module.exports = router;
