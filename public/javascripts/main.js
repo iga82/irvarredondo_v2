178f66ba40d5dbc06d20f034f73b7d024993a441
Irving Arredondo <irva91@gmail.com>
2016-01-11 02:59:36 -0600

    finished redesign

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,49 +1,7 @@
 // document ready
 $(function(){
 	// fade in profile picture
-	$("#home-div").fadeIn(3000);
-
-	// about-link
-	$('#about-link').click(function(){
-		$('#about-div').show();
-		console.log("About page shown");
-
-		// hide the rest of the partials
-		$('#home-div').hide();
-		$('#contact-div').hide();
-		$('#portfolio-div').hide();
-	});
-
-	// contact-link
-	$('#contact-link').click(function(){
-		$('#contact-div').show();
-		console.log("Contact page shown");
-
-		// hide the rest of the partials
-		$('#home-div').hide();
-		$('#about-div').hide();
-		$('#portfolio-div').hide();
-	});
-
-	// portfolio-link
-	$('#portfolio-link').click(function(){
-		$('#portfolio-div').show();
-		console.log("Portfolio page shown");
-
-		// hide the rest of the partials
-		$('#home-div').hide();
-		$('#contact-div').hide();
-		$('#about-div').hide();
-	});
-
-	// home-link
-	$('#home-link').click(function(){
-		$('#home-div').show();
-		console.log("Portfolio page shown");
-
-		// hide the rest of the partials
-		$('#portfolio-div').hide();
-		$('#contact-div').hide();
-		$('#about-div').hide();
+	$("#home-div").fadeIn(3000, function(){
+		$(".fade-in-after-profile").fadeIn(500);
 	});
 });
