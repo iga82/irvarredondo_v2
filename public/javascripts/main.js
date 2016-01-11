30f8eda857b228c7c848081e3cad62cca0242daf
Irving Arredondo <igarredo@us.ibm.com>
2016-01-11 15:35:17 -0600

    added javascript to help with resizing icons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,44 @@
 // document ready
 $(function(){
+
+	// check resize
+	$( window ).resize(function() {
+		var width = $(window).width();
+		if(width < 415){
+			$("#links-div-normal").hide();
+			$("#small-image-div-offset").removeClass("col-xs-offset-4");
+			$("#small-image-div-offset").addClass("col-xs-offset-3");
+			$("#links-div-small").show();
+
+		}
+		else if(width > 415 && width < 650){
+			$("#links-div-normal").hide();
+			$("#small-image-div-offset").removeClass("col-xs-offset-3");
+			$("#small-image-div-offset").addClass("col-xs-offset-4");
+			$("#links-div-small").show();
+		}
+		else if(width > 650){
+			$("#links-div-small").hide();
+			$("#links-div-normal").show();
+		}
+	});
+
 	// fade in profile picture
 	$("#home-div").fadeIn(3000, function(){
+		var width = $(window).width();
 		$(".fade-in-after-profile").fadeIn(500);
+		if(width > 650){
+			console.log("Width size, ok to show contact links");
+			$("#links-div-normal").fadeIn(500);
+		}
+		else if(width < 650 && width > 415){
+			$("#links-div-small").fadeIn(500);
+		}
+		else if(width < 415){
+			// need to adjust offset for screens less than 415
+			$("#small-image-div-offset").removeClass("col-xs-offset-4");
+			$("#small-image-div-offset").addClass("col-xs-offset-3");
+			$("#links-div-small").fadeIn(500);
+		}
 	});
 });
